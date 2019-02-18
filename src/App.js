5000680bed23437870a260d43b676e5703693381
Raphael Seguin <raph4l3@gmail.com>
2019-02-18 11:24:15 +0100

    reactstrap ajouté, premières questions faites, ej comprend toujours pas pourquoi ça ne fonctionne aps correctement, je poserais els questions cet aprem

--- src/App.js
+++ src/App.js
@@ -3,20 +3,27 @@ import Header from './container/Header';
 import SideMenu from './container/SideMenu';
 import Artist from './component/Artist';
 import AlbumGrid from './container/AlbumGrid';
+import {Container, Row, Col } from 'reactstrap';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Container>
         <Header />
+          <Row>
+              <Col xs = "6" md="4" lg = "2">
         <SideMenu />
+              </Col>
+              <Col xs = "6" md = "8" lg = "10">
         <main>
           <Artist />
           <AlbumGrid />
         </main>
-      </div>
+              </Col>
+          </Row>
+      </Container>
     );
   }
 }
